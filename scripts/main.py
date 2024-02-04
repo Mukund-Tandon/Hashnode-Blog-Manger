3096a972a145d1e66be513f7e36a219dfa03ffd1
Mukund-Tandon <mukundtandon1211@gmail.com>
2024-02-04 19:10:29 +0530

    added  files

--- scripts/main.py
+++ scripts/main.py
@@ -2,6 +2,7 @@ import sys
 def process_content():
     for line in sys.stdin:
         # Process each line of input as needed
+        print("1111111111111111")
         print(line.strip())  # Example: Print the stripped content of each line
 
 if __name__ == "__main__":
